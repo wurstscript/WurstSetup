2cc0fdc262511fce2c699a9734cc96087ef0e4e0
Frotty <frotty@sunayama.de>
2019-02-09 12:02:16 +0100

    Update InstallationTests.kt

--- src/test/kotlin/InstallationTests.kt
+++ src/test/kotlin/InstallationTests.kt
@@ -3,13 +3,13 @@ import global.InstallationManager
 import org.testng.annotations.Test
 
 
-@Test
+@Test(priority = 8)
 fun testVersionPattern() {
     assert(InstallationManager.isJenkinsBuilt("1.7.0.0-jenkins-Wurst-531"))
 
 }
 
-@Test
+@Test(priority = 8)
 fun testInstall() {
 	InstallationManager.getVersionFomJar()
 
