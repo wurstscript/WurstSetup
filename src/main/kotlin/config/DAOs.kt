09fec61e12b2c54cedf3052a2ab070a9b53b8f0a
Frotty <frotty@sunayama.de>
2019-03-14 23:50:13 +0100

    Don't output default values

--- src/main/kotlin/config/DAOs.kt
+++ src/main/kotlin/config/DAOs.kt
@@ -1,5 +1,6 @@
 package config
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -7,6 +8,7 @@ import kotlin.collections.ArrayList
  * The root DAO that contains the child DAOs.
  * Represents a complete wurst.build file.
  */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectConfigData(var projectName: String = "DefaultName",
                                   val dependencies: ArrayList<String> = ArrayList(Arrays.asList("https://github.com/wurstscript/wurstStdlib2")),
 //                                  val jobs: ArrayList<WurstProjectBuildJob> =
@@ -17,6 +19,7 @@ data class WurstProjectConfigData(var projectName: String = "DefaultName",
                                   val buildMapData: WurstProjectBuildMapData = WurstProjectBuildMapData())
 
 /** All data needed to generate the output map */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildMapData(val name: String = "JustAnotherWurstMap",
                                     val fileName: String = "MyWurstMap",
                                     val author: String = "SomeWurstUser",
@@ -26,21 +29,25 @@ data class WurstProjectBuildMapData(val name: String = "JustAnotherWurstMap",
                                     val forces: ArrayList<WurstProjectBuildForce> = ArrayList())
 
 /** Wurst job data with list of arguments */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildJob(val name: String = "DefaultJobName",
                                 val args: ArrayList<String> = ArrayList(Arrays.asList()))
 
 /** Scenario related information */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildScenarioData(val description: String = "WurstScript powered!",
                                          val suggestedPlayers: String = "DefaultSuggestedPlayers",
                                          var loadingScreen: WurstProjectBuildLoadingScreenData?)
 
 /** Load screen information. */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildLoadingScreenData(val model: String = "Generic.mdx",
                                               val title: String = "DefaultTitle",
                                               val subTitle: String = "DefaultSubTitle",
                                               val text: String = "DefaultText")
 
 /** Map build flags */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildOptionFlagsData(val hideMinimapPreview: Boolean = false,
                                             val maskedAreasPartiallyVisible: Boolean = true,
                                             val showWavesOnCliffShores: Boolean = true,
@@ -48,6 +55,7 @@ data class WurstProjectBuildOptionFlagsData(val hideMinimapPreview: Boolean = fa
                                             val useItemClassificationSystem: Boolean = true)
 
 /** Data for one force (team) in the map */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildForce(val name: String = "DefaultForce",
                                   val flags: WurstProjectBuildForceFlags = WurstProjectBuildForceFlags(),
                                   val playerIds: IntArray = intArrayOf(0)) {
@@ -74,6 +82,7 @@ data class WurstProjectBuildForce(val name: String = "DefaultForce",
 }
 
 /** Force flags */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildForceFlags(val allied: Boolean = true,
                                        val alliedVictory: Boolean = true,
                                        val sharedVision: Boolean = true,
@@ -91,6 +100,7 @@ enum class Controller {
 }
 
 /** Data for one player */
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 data class WurstProjectBuildPlayer(val id: Int = 0,
                                    val name: String = "DefaultPlayer",
                                    val race: Race = Race.SELECTABLE,
