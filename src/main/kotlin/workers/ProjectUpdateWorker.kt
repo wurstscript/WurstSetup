1bcbd6cbdd3d7d90703d71fa1f38b50d6dd3fec6
Frotty <Frotty@users.noreply.github.com>
2026-03-03 18:08:50 +0100

    Update ProjectUpdateWorker.kt

--- src/main/kotlin/workers/ProjectUpdateWorker.kt
+++ src/main/kotlin/workers/ProjectUpdateWorker.kt
@@ -5,7 +5,7 @@ import java.nio.file.Path
 import javax.swing.SwingWorker
 
 /** Handles updating an existing project */
-class ProjectUpdateWorker(private val projectRoot: Path, private val gamePath: Path, private val configData: WurstProjectConfigData) : SwingWorker<Boolean, Void>() {
+class ProjectUpdateWorker(private val projectRoot: Path, private val gamePath: Path?, private val configData: WurstProjectConfigData) : SwingWorker<Boolean, Void>() {
 
     @Throws(Exception::class)
     override fun doInBackground(): Boolean? {
