1c967494b1d1821ba52a67d7ca6a0c9bc855cd06
Frotty <Frotty@users.noreply.github.com>
2026-04-30 12:41:05 +0200

    fk

--- src/test/kotlin/ConnectivityTests.kt
+++ src/test/kotlin/ConnectivityTests.kt
@@ -5,13 +5,13 @@ import org.testng.annotations.Test
 class ConnectivityTests {
 
 		@Test fun testConnectionManager() {
-			assert(ConnectionManager.checkConnectivity("http://google.com") == NetStatus.SERVER_CONTACT)
+			assert(ConnectionManager.checkConnectivity("http://google.com") == NetStatus.SERVER_CONTACT) { "google.com unreachable" }
 
-			assert(ConnectionManager.checkWurstBuild() == NetStatus.ONLINE)
+			assert(ConnectionManager.checkWurstBuild() == NetStatus.ONLINE) { "wurst build unreachable, status=${ConnectionManager.netStatus}" }
 
-			assert(ConnectionManager.getLatestCompilerBuild() > 20250000)
+			assert(ConnectionManager.getLatestCompilerBuild() > 20250000) { "unexpected build date: ${ConnectionManager.getLatestCompilerBuild()}" }
 
-			assert(ConnectionManager.getLatestSetupBuild() == 0)
+			assert(ConnectionManager.getLatestSetupBuild() == 0) { "expected 0 but got ${ConnectionManager.getLatestSetupBuild()}" }
 		}
 
 }
